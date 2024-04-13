f012f32f713659b37b9398383b801fbe7638d7f5
Osama Aljamal <osamaelgamal39@yahoo.com>
2024-04-13 13:54:09 -0400

    Print churn ratio graphically

--- Main.py
+++ Main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.ticker as mtick
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math
 
 dataset = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
@@ -6,3 +11,9 @@ print(dataset.columns)
 
 print(dataset.values[0:6])
 
+#Apply the ggplot style
+plt.style.use("ggplot")
+plt.figure(figsize=(5,5))
+ax = sns.countplot(x = dataset["Churn"],palette="Blues")
+ax.bar_label(ax.containers[0])
+plt.show()
