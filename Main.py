ff4cdcea044ac89569cb76826df756db1de17662
Greg Salisbury <gregsalisbury25@gmail.com>
2024-04-13 14:38:18 -0400

    created histogram for employee tenure

--- Main.py
+++ Main.py
@@ -11,9 +11,24 @@ print(dataset.columns)
 
 print(dataset.values[0:6])
 
-#Apply the ggplot style
+# Apply the ggplot style
+
 plt.style.use("ggplot")
-plt.figure(figsize=(5,5))
-ax = sns.countplot(x = dataset["Churn"],palette="Blues")
+plt.figure(figsize=(5, 5))
+ax = sns.countplot(x=dataset["Churn"], palette="Blues")
 ax.bar_label(ax.containers[0])
+plt.show()
+
+print(dataset.columns[5])
+
+firstCol = []
+
+for row in dataset.values:
+    row = list(row)
+    firstCol.append(row[5])
+
+plt.hist(firstCol, density=True, bins=10)
+plt.xlabel('Tenure (years)')
+plt.ylabel('Fraction (per year)')
+plt.title('Histogram of Employee Tenure')
 plt.show()
